5de1697f2b4af1a187b4df411f1bee5e6c049c3c
James Dabbs <jamesdabbs@gmail.com>
2013-02-25 22:24:15 -0500

    Empty project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Brubeck::Application.config.secret_key_base = 'b8f874c70a7f741e9b8e065ea97af0a139991da1d33a65198ecb873ce68e7c78150331bd4b08f43146d27b44a8c0efdb543b0e08fb9caa6d8194849eb614ad0d'
