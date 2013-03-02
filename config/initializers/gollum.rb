4357cc82b3f3d1cfb9ab4a86373267e5653fc6a2
James Dabbs <jamesdabbs@gmail.com>
2013-03-02 18:54:23 -0500

    Enable MathJax

--- config/initializers/gollum.rb
+++ config/initializers/gollum.rb
@@ -4,4 +4,4 @@ gollum_path = File.expand_path '../../../wiki', __FILE__
 
 Precious::App.set :gollum_path, gollum_path
 Precious::App.set :default_markup, :markdown
-Precious::App.set :wiki_options, universal_toc: false
+Precious::App.set :wiki_options, universal_toc: false, mathjax: true
