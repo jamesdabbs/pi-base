0a69e0648f47f0c2296e720ceb27584170ca9d07
James Dabbs <jamesdabbs@gmail.com>
2013-03-02 17:24:06 -0500

    Replace trait index with trait table

--- app/controllers/traits_controller.rb
+++ app/controllers/traits_controller.rb
@@ -1,6 +1,15 @@
 class TraitsController < ApplicationController
   def index
-    @traits = Trait.paginate page: params[:page], per_page: 100
+    @spaces     = Space.order('id ASC').pluck :id
+    @properties = Property.order('id ASC').pluck :id
+
+    values = {}
+    Value.all.each { |v| values[v.id] = v.name.sub('True', '+').sub('False', '-') }
+
+    @traits = Hash[ @spaces.map { |s| [s,{}] } ]
+    Trait.select(:id, :space_id, :property_id, :value_id).each do |t|
+      @traits[t.space_id][t.property_id] ||= [t.id, values[t.value_id]]
+    end
   end
 
   def show
