62ee9eaeaa5a1f59b1264af94c1afa6794e6fc97
James Dabbs <jamesdabbs@gmail.com>
2013-03-02 21:38:48 -0500

    Use Trait text from wiki

--- app/models/trait.rb
+++ app/models/trait.rb
@@ -2,4 +2,10 @@ class Trait < ActiveRecord::Base
   belongs_to :space
   belongs_to :property
   belongs_to :value
+
+  include Wiki
+
+  def name
+    "#{space} - #{property}"
+  end
 end
--- config/initializers/gollum.rb
+++ config/initializers/gollum.rb
@@ -15,3 +15,13 @@ Precious::App.set :wiki_options, universal_toc: false, mathjax: true
 Gollum::Markup.register(:v, "Coq") do |content|
   "<pre>#{CGI::escapeHTML content}</pre>"
 end
+
+Gollum::Markup.register(:bmd, "Brubeck") do |content|
+  text = ""
+  content.split(',').each do |fact|
+    fact.strip!
+    next if fact.empty?
+    text << "* #{fact}\n"
+  end
+  GitHub::Markdown.render text
+end
