8d0cc05cc1491a71f0c2a434c2a7978de7468dc4
James Dabbs <jamesdabbs@gmail.com>
2013-04-25 23:19:56 -0400

    Add Theorems to Property show view

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,3 +1,8 @@
 class PropertiesController < ObjectsController
   @object_class = Property
+
+  def show
+    super
+    @theorems = object.theorems.paginate page: params[:theorems],  per_page: 15
+  end
 end
