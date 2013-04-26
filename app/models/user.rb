11cbd5eb4d42c117a21b2cf95595fcfecde2b610
James Dabbs <jamesdabbs@gmail.com>
2013-04-25 20:56:28 -0400

    Restore devise's :registerable
    We need it for the user edit view

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  # Intentionally disabled (for now):
-  # :registerable
-  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 end
