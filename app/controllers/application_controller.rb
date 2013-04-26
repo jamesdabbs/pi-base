7624698af8fc91018086b0c4483c30925fb0e4ac
James Dabbs <jamesdabbs@gmail.com>
2013-04-26 12:28:47 -0400

    Catch and redirect authorization errors to login; show flash

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to new_user_session_url, alert: exception.message
+  end
+
   def root
   end
 
