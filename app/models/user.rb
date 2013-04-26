e34561b45e4002435fdceaaf8aa20e2d27177c1c
James Dabbs <jamesdabbs@gmail.com>
2013-04-25 20:49:54 -0400

    Remove signups; customize Devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  # Intentionally disabled (for now):
+  # :registerable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
 end
