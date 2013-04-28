e83ba3d39e16d2f867700e4df84431d1d5578d11
James Dabbs <jamesdabbs@gmail.com>
2013-04-28 06:00:56 -0400

    Add node description on mouseover. Fixes #10

--- app/models/proof/tree.rb
+++ app/models/proof/tree.rb
@@ -5,12 +5,22 @@ class Proof
     end
 
     def as_json opts={}
-      nodes = @space.traits.map do |trait|
-        {
-          name:    trait.assumption_description,
-          id:      trait.id,
-          deduced: trait.deduced?
+      # FIXME: cache by space
+      # FIXME: use rabl?
+      nodes = @space.traits.includes(:proof).map do |trait|
+        node = {
+          name: trait.assumption_description,
+          id:   trait.id
         }
+        if trait.proof
+          node[:theorem] = {
+            name: trait.proof.theorem.name,
+            id:   trait.proof.theorem_id
+          }
+        else
+          node[:description] = trait.description
+        end
+        node
       end
 
       node_index = Hash[ nodes.each_with_index.map { |n,i| [n[:id], i] } ]
