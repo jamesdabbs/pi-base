16762b072a2d320159fe229912e3f6fda7038cc2
James Dabbs <jamesdabbs@gmail.com>
2013-05-19 09:49:40 -0400

    Allow creating properties; default to boolean-valued

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -5,4 +5,10 @@ class PropertiesController < ObjectsController
     super
     @theorems = object.theorems.paginate page: params[:theorems],  per_page: 15
   end
+
+  private #-----
+
+  def create_params
+    params.require(object_name).permit :name, :description, :value_set_id
+  end
 end
