8d364be0064e8825eb1b354e099b94367cf224dd
James Dabbs <jamesdabbs@gmail.com>
2013-05-19 09:21:29 -0400

    Check proof table against counterexamples

--- app/models/trait/table.rb
+++ app/models/trait/table.rb
@@ -1,3 +1,5 @@
+require 'csv'
+
 class Trait
   class Table
     attr_reader :spaces, :properties
@@ -22,6 +24,33 @@ class Trait
         properties: properties,
         traits:     traits
       }.to_json
+    end
+
+    def check
+      csv = CSV.read Rails.root.join 'counterexamples.csv'
+      space_map    = Hash[ csv.each_with_index.map { |r,i| [r[0], i] } ]
+      property_map = Hash[ csv.first.each_with_index.map { |c,i| [c, i] } ]
+      value_map    = { "0" => "False", "1" => "True" }
+
+      space_ids    = @spaces.map &:id
+      property_ids = @properties.map &:id
+      traits = Trait.
+        where(space_id: space_ids, property_id: property_ids).
+        includes(:space, :property, :value).
+        order(:space_id)
+
+      traits.each do |t|
+        # Look up trait value in table and check
+        row = space_map[t.space.name]
+        col = property_map[t.property.name]
+        next unless row && col
+        csv_val  = csv[row][col]
+        expected = value_map[csv_val]
+        if expected && t.value.name != expected
+          puts "#{t.space.name} has #{t.property.name} #{t.value.name}, expected #{expected}"
+        end
+      end
+      nil
     end
   end
 end
